3632d6ee8548746658f6fd9d3a87c2951751c2bc
luisecarrillom <luisecarrillom@gmail.com>
2025-06-18 20:12:03 -0600

    Create Card component with image click handler and prop validation

---
+++ 10_02/Card.jsx
@@ -0,0 +1,36 @@
+// File: Card.jsx
+
+import React from "react";
+import PropTypes from "prop-types";
+
+// Simple style for clickable image
+const clickableStyle = { cursor: "pointer", border: "2px solid blue" };
+
+// React functional component
+const Card = ({ id, title, image, onImageClick }) => {
+  const handleClick = (event) => {
+    onImageClick(event, { id, title });
+  };
+
+  return (
+    <div className="card" style={{ padding: "1rem", textAlign: "center" }}>
+      <h2>{title}</h2>
+      <img
+        src={image}
+        alt={title}
+        style={clickableStyle}
+        onClick={handleClick}
+      />
+    </div>
+  );
+};
+
+// Prop validation
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  onImageClick: PropTypes.func.isRequired,
+};
+
+export default Card;
